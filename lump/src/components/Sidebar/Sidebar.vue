d29b14295b79058cb1d6c12c1c1f7895c46809c4
kristinee3 <christineyema@hotmail.com>
2017-08-08 12:20:59 +0200

    Sidebar-position

--- lump/src/components/Sidebar/Sidebar.vue
+++ lump/src/components/Sidebar/Sidebar.vue
@@ -3,9 +3,24 @@
 
 <!-- HTML -->
 <template>
-  <div id="sidebar">
-    <div>Un composant personnalisé sidebar !</div>
-  </div>
+	<div id="sidebar">
+		<div class="container">
+			<div class="navbar-sidebar"> 
+				<div class="player">
+					<i class="fa fa-align-center fa-lg" @click="showId='player'"></i>
+				</div>
+				<div class="notifications">
+					<i class="fa fa-bell fa-lg" @click="showId='notifications'"></i>
+				</div>
+				<div class="chat">
+					<i class="fa fa-comment fa-lg" @click="showId='chat'"></i>
+				</div>
+			</div>
+			<p v-if="showId === 'player'">Hello</p>
+			<p v-if="showId === 'notifications'">i'm here</p>
+			<p v-if="showId === 'chat'">and here to</p>
+		</div>
+	</div>
 </template>
 
 <!-- SCRIPT -->
@@ -15,10 +30,10 @@
 export default {
   
   name: 'sidebar',
-  data() {
+  data: function () {
     return {
-       msg: '',
-    }
-  },
+      showId:false,
+    }    
+  }
 }
 </script>

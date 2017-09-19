c83b3253a3cad8a127d92128bbfdbd666773c30e
LolaCalmejane <lola.calmejane@hetic.net>
2017-09-19 15:59:34 +0200

    login debug css

--- lump/src/components/Login/Login.vue
+++ lump/src/components/Login/Login.vue
@@ -4,20 +4,21 @@
 <!-- HTML -->
 <template>
 	<div id="login" class="login">
-		<div class="content">
+		<div class="">
 			<img src="../../assets/images/login-logo.png" alt="Logo-lump">
 			<h2 class="subtitle-white title-connexion">Connexion</h2>
-			<form>
+			<form v-on:submit.prevent="connexion(Result)">
 				<div class="form-connexion">
 			        <input type="text" v-model.trim="loginIt.login" class="pseudo" placeholder="Pseudo" required>
 			        <input type="password" class="mdp" v-model.trim="loginIt.password" placeholder="Mot de passe" required>
-			        <button class="buttonConnexion subtitle-white" @click.prevent="connexion(Result)">Se connecter</button>
+			        <button class="buttonConnexion subtitle-white">Se connecter</button>
 	      		</div>
+	      	</form>
 	      		<div class="ifNot">
 		      		<p class="detail">Vous n’êtes pas encore inscrit ?</p>
 		      		<router-link to ="/inscription"><p class="body-text">S’inscrire</p></router-link>
 	      		</div>
-	      	</form>
+	      	
       </div>
 	</div>
 </template>
@@ -38,7 +39,7 @@ import axios from 'axios'
 		},
 		methods: {
             connexion(Result) {
-                axios.get("http://localhost:3000/api/1.0/user/login?authorization="+ btoa(this.loginIt.login+':'+this.loginIt.password))
+                axios.get("http://localhost:3000/api/1.0/user/login?authorization="+ btoa(this.loginIt.login+':'+this.loginIt.password), this.Result)
             	.then((response) =>{ 
             		console.log(response.data);
 					this.Result = response.data.result;

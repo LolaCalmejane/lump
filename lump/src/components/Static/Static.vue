51811e346544a5a68107797a8afeb77ace6714c1
kristinee3 <christineyema@hotmail.com>
2017-09-20 12:31:22 +0200

    Static changes

--- lump/src/components/Static/Static.vue
+++ lump/src/components/Static/Static.vue
@@ -7,23 +7,23 @@
     <div id="static">
         <nav class="global-nav circleBehind">
             <div class="content">
-            <ul>
-                <img id="brand" src="../../assets/images/img-logo.png" alt="Logo">
-                <search></search>
-                <!-- <input type="text" name="search" class="subtitle-white" placeholder="Rechercher"> -->
-                <li><router-link to ="/create" class="subtitle-white">Créer</router-link></li>
-                <li><router-link to ="/events" class="subtitle-white">A venir</router-link></li>
-                <li><router-link to ="/profil" class="subtitle-white">Jude Ademilo</router-link></li>
-                <li class="dropdown subtitle-white">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <span class="caret"></span></a>
-                <ul class="dropdown-menu dropdown-menu-right">
-                    <li><a class="subtitle-white" href="#">Modifier profil</a></li>
-                    <li><a class="subtitle-white" href="#">Déconnexion</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a class="subtitle-white" href="#">Paramètres</a></li>
+                <ul>
+                    <router-link to ="/profil" class="subtitle-white"><img id="brand" src="../../assets/images/img-logo.png" alt="Logo"></router-link>
+                    <search></search>
+                    <!-- <input type="text" name="search" class="subtitle-white" placeholder="Rechercher"> -->
+                    <li><router-link to ="/create" class="subtitle-white">Créer</router-link></li>
+                    <li><router-link to ="/events" class="subtitle-white">Événements</router-link></li>
+                    <li><router-link to ="/profil" class="subtitle-white">Jude Ademilo</router-link></li>
+                    <!--li class="dropdown subtitle-white">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <span class="caret"></span></a>
+                    <!--ul class="dropdown-menu dropdown-menu-right">
+                        <!--li><a class="subtitle-white" href="#">Modifier profil</a></li-->
+                        <!--li><a class="subtitle-white" href="#">Déconnexion</a></li-->
+                        <!--li role="separator" class="divider"></li-->
+                        <!--li><a class="subtitle-white" href="#">Paramètres</a></li-->
+                    <!--/ul-->
+                    <!--/li-->
                 </ul>
-                </li>
-            </ul>
             </div>
         </nav>
         <sidebar></sidebar>
